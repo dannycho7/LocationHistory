ca3082e90ea98b72ae9cafe511a24707eb59fbc3
dannycho7 <dannycho7@gmail.com>
2017-09-22 22:49:14 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,40 @@
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+var chrome = require('selenium-webdriver/chrome');
+
+var path = require('chromedriver').path;
+
+var service = new chrome.ServiceBuilder(path).build();
+chrome.setDefaultService(service);
+
+var driver = new webdriver.Builder()
+    .withCapabilities(webdriver.Capabilities.chrome())
+    .build();
+
+driver.get("https://google.com");
+
+driver.findElement(By.id("gb_70")).click()
+driver.findElement(By.name("identifier")).sendKeys("crazycandy15@gmail.com");
+driver.findElement(By.id("identifierNext")).click();
+/*driver.sleep(500);
+driver.findElement(By.name("password")).sendKeys("");
+driver.findElement(By.id("passwordNext")).click();
+*/
+driver.wait(until.elementLocated(By.name("q")))
+.then(element => {
+	driver.get('https://takeout.google.com/settings/takeout/custom/location_history?expflags&gl=US&hl=en');
+	driver.findElement(By.xpath("//*[text() = 'Next']")).click();
+	driver.sleep(1000);
+	driver.wait(until.elementLocated(By.xpath("//*[text() = 'Next']")))
+	.then(element => {
+		driver.findElement(By.xpath("//*[text() = 'Create archive']")).click();
+
+		driver.wait(until.elementLocated(By.xpath(`//*[text() = '<div class="Hfa"></div>Download']`)))
+		.then(element => {
+			driver.sleep(100);
+			element.click();
+			driver.quit();
+		});
+	});
+});
