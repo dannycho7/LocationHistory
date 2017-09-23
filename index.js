b6e7bd0b16f8f2b5b7628d5bef82d5e402acfdf0
dannycho7 <dannycho7@gmail.com>
2017-09-23 08:59:54 -0700

    Download button works - saves to output directory

--- index.js
+++ index.js
@@ -8,14 +8,19 @@ var path = require('chromedriver').path;
 var service = new chrome.ServiceBuilder(path).build();
 chrome.setDefaultService(service);
 
+var options = new chrome.Options();
+let downloadPath = __dirname + "/output";
+options.setUserPreferences({ "download.default_directory": downloadPath });
+
 var driver = new webdriver.Builder()
-    .withCapabilities(webdriver.Capabilities.chrome())
+	.forBrowser("chrome")
+    .withCapabilities(options.toCapabilities())
     .build();
 
 driver.get("https://google.com");
 
 driver.findElement(By.id("gb_70")).click()
-driver.findElement(By.name("identifier")).sendKeys("crazycandy15@gmail.com");
+driver.findElement(By.name("identifier")).sendKeys("dannycho91@gmail.com");
 driver.findElement(By.id("identifierNext")).click();
 /*driver.sleep(500);
 driver.findElement(By.name("password")).sendKeys("");
@@ -23,17 +28,18 @@ driver.findElement(By.id("passwordNext")).click();
 */
 driver.wait(until.elementLocated(By.name("q")))
 .then(element => {
-	driver.get('https://takeout.google.com/settings/takeout/custom/location_history?expflags&gl=US&hl=en');
+	driver.get("https://takeout.google.com/settings/takeout/custom/location_history?expflags&gl=US&hl=en");
 	driver.findElement(By.xpath("//*[text() = 'Next']")).click();
 	driver.sleep(1000);
 	driver.wait(until.elementLocated(By.xpath("//*[text() = 'Next']")))
 	.then(element => {
 		driver.findElement(By.xpath("//*[text() = 'Create archive']")).click();
 
-		driver.wait(until.elementLocated(By.xpath(`//*[text() = '<div class="Hfa"></div>Download']`)))
+		driver.wait(until.elementLocated(By.css(".do.am div[jscontroller='vHpMNe']")))
 		.then(element => {
 			driver.sleep(100);
 			element.click();
+			driver.sleep(10000);
 			driver.quit();
 		});
 	});
