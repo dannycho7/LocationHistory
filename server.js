aa287d394e26b7b84811d2cb93ac9e1633419aa1
dannycho7 <dannycho7@gmail.com>
2017-09-23 13:06:50 -0700

    Add server component to utilize the script

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+const scraperMethods = require(".");
+
+app.use(bodyParser.json());
+
+app.post("*", (req, res) => {
+	if(req.body["email"] && req.body["password"]) {
+		scraperMethods.grabTakeout(req.body["email"], req.body["password"])
+		.then(scraperMethods.unzipForLocationJSON)
+	}
+});
+
+app.listen(5000, () => console.log("Server listening in on port 5000"));
