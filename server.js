2c0949ca073a31ebbaf7556804c1f419bed0e844
dannycho7 <dannycho7@gmail.com>
2017-09-23 23:51:10 -0700

    Add rough calculations for inconvenience factor and send back based on margin of error threshold

--- server.js
+++ server.js
@@ -16,14 +16,21 @@ const scraperMethods = require(".");
 app.use(bodyParser.json());
 
 app.post("/", (req, res) => {
+	console.log("Received root request");
 	if(req.body["email"] && req.body["password"]) {
 		scraperMethods.grabTakeout(req.body["email"], req.body["password"])
 		.then(scraperMethods.unzipForLocationJSON(req.body["email"], (data) => {
 			User.create({ email: req.body["email"], routes: data });
-			res.end(data.slice(0, 50));
+			res.end(JSON.stringify(data.slice(0, 50)));
 		}));
 	}
 });
+
+function computeInconvenience(currentUserFastRoute, destination) {
+	return Math.abs(currentUserFastRoute[0] - destination[0]) + Math.abs(currentUserFastRoute[1] - destination[1]);
+}
+
+const marginOfError = 0.5;
 
 app.post("/compute", (req, res) => {
 	console.log("Computing...");
@@ -31,10 +38,24 @@ app.post("/compute", (req, res) => {
 		User.find({ email: req.body["email"] })
 		.then(user => {
 			if(user.length > 0) {
-				let fastRoutes = user[0]["routes"];
-				console.log("found", fastRoutes);
-				db.collection("locations").find().toArray((err, locations) => {
-					console.log("All locations", locations);
+				let currentUserFastRoutes = user[0]["routes"];
+				console.log("found", currentUserFastRoutes);
+				db.collection("users").find({ "email": { $ne: req.body["email"] } }).toArray((err, users) => {
+					let roughInconvenience = {};
+					users.forEach(user => {
+						user["routes"].forEach(destination => {
+							currentUserFastRoutes.forEach(currentUserFastRoute => {
+								let inconvenience = computeInconvenience(currentUserFastRoute, destination);
+								console.log(`Inconvenience is ${inconvenience} for ${currentUserFastRoute} and ${destination}`)
+								if(inconvenience < marginOfError) {
+									roughInconvenience[inconvenience1] = destination;
+								}
+							});
+						});
+						
+					});
+					console.log(`Sending back rough inconvenience ${JSON.stringify(roughInconvenience)}`);
+					res.end(JSON.stringify(roughInconvenience));
 				});
 			}
 		});
