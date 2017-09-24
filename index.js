dda1ab0a09f6e1555dd7e7a623b4ec9e2041b743
dannycho7 <dannycho7@gmail.com>
2017-09-23 23:50:17 -0700

    Move chromedriver setup outside of method call

--- index.js
+++ index.js
@@ -5,15 +5,14 @@ const unzip = require("unzip2");
 const webdriver = require('selenium-webdriver');
 const { By, until } = webdriver;
 const chrome = require('selenium-webdriver/chrome');
+const service = new chrome.ServiceBuilder(require('chromedriver').path).build();
+chrome.setDefaultService(service);
+var options = new chrome.Options();
+let downloadPath = __dirname + "/output";
+options.setUserPreferences({ "download.default_directory": downloadPath });
 
 module.exports.grabTakeout = async function grabTakeout(email, password) {
-	const service = new chrome.ServiceBuilder(require('chromedriver').path).build();
-	chrome.setDefaultService(service);
-
-	var options = new chrome.Options();
-	let downloadPath = __dirname + "/output";
-	options.setUserPreferences({ "download.default_directory": downloadPath });
-
+	
 	var driver = new webdriver.Builder()
 					.forBrowser("chrome")
 				    .withCapabilities(options.toCapabilities())
